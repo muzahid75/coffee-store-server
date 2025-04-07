bf6da6d8ef3f1dce7d83d10e42a9dd84831c39c8
muzahid75 <eng.md.muzahid@gmail.com>
2025-04-07 16:52:55 +0600

    deployment

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-module.exports=app;
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+module.exports = app;
